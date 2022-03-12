4ae5136c0f4f7c1927be8388ae03e42627871b90
Abdullah Al Mamun <almamun00021@gmail.com>
2022-03-12 21:08:29 +0600

    table added

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Icons CSS -->
     <link rel="stylesheet" href="{{ asset('css/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap4.css') }}">
     <!-- Date Range Picker CSS -->
     <link rel="stylesheet" href="{{ asset('css/daterangepicker.css') }}">
     <!-- App CSS -->
