101930568b7ad8b20c0947e6abe52a6a23c1a376
Abdullah Al Mamun <almamun00021@gmail.com>
2022-03-13 22:59:45 +0600

    charts added

--- resources/views/admin/charts/datamaps.blade.php
+++ resources/views/admin/charts/datamaps.blade.php
@@ -1,17 +1,61 @@
 @extends('layouts.app')
 
 @section('title')
-    Home
+    Datamaps
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Home</h3>
+<div class="row justify-content-center">
+    <div class="col-12 col-lg-10 col-xl-10">
+      <h2 class="page-title">Datamaps</h2>
+      <p class="text-muted">Customizable SVG map visualizations for the web in a single Javascript file using D3.js</p>
+      <div class="row">
+        <div class="col-12 mb-4">
+          <div class="card shadow">
+            <div class="card-body mt-n3">
+              <h6 class="card-title my-3">World Datamap</h6>
+              <div id="dataMapWorld" style="position: relative; width: 100%; height: 300px;"></div>
+            </div>
+          </div>
         </div>
-
-        <div class="card-body">
-
+        <div class="col-md-6 mb-4">
+          <div class="card shadow">
+            <div class="card-header">
+              <strong class="card-title">USA Datamap</strong>
+            </div>
+            <div class="card-body my-n3">
+              <div id="dataMapUSA" style="position: relative; width: 100%; height: 200px;"></div>
+            </div>
+          </div>
         </div>
+        <div class="col-md-6 mb-4">
+          <div class="card shadow">
+            <div class="card-header">
+              <strong class="card-title">Europe Datamap</strong>
+            </div>
+            <div class="card-body my-n3">
+              <div id="dataMapEurope" style="position: relative; width: 100%; height: 200px;"></div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- end section -->
     </div>
+  </div> <!-- .row -->
 @endsection
+@section('scripts')
+    <script src="{{ asset('js/d3.min.js') }}"></script>
+    <script src="{{ asset('js/topojson.min.js') }}"></script>
+    <script src="{{ asset('js/datamaps.all.min.js') }}"></script>
+    <script src="{{ asset('js/datamaps-zoomto.js') }}"></script>
+    <script src="{{ asset('js/datamaps.custom.js') }}"></script>
+
+    <script>
+        /* defind global options */
+        Chart.defaults.global.defaultFontFamily = base.defaultFontFamily;
+        Chart.defaults.global.defaultFontColor = colors.mutedColor;
+    </script>
+    <script src="{{ asset('js/gauge.min.js') }}"></script>
+    <script src="{{ asset('js/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('js/apexcharts.custom.js') }}"></script>
+@endsection
+
