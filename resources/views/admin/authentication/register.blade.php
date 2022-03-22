850129e924d192448afa8e77832bdddbaec34ea9
Abdullah Al Mamun <almamun00021@gmail.com>
2022-03-22 21:39:45 +0600

    auth pages add

--- resources/views/admin/authentication/register.blade.php
+++ resources/views/admin/authentication/register.blade.php
@@ -8,16 +8,16 @@
     <link rel="icon" href="favicon.ico">
     <title>Tiny Dashboard - A Bootstrap Dashboard Template</title>
     <!-- Simple bar CSS -->
-    <link rel="stylesheet" href="css/simplebar.css">
+    <link rel="stylesheet" href="{{asset('css/simplebar.css')}}">
     <!-- Fonts CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- Icons CSS -->
-    <link rel="stylesheet" href="css/feather.css">
+    <link rel="stylesheet" href="{{asset('css/feather.css')}}">
     <!-- Date Range Picker CSS -->
-    <link rel="stylesheet" href="css/daterangepicker.css">
+    <link rel="stylesheet" href="{{asset('css/daterangepicker.css')}}">
     <!-- App CSS -->
-    <link rel="stylesheet" href="css/app-light.css" id="lightTheme">
-    <link rel="stylesheet" href="css/app-dark.css" id="darkTheme" disabled>
+    <link rel="stylesheet" href="{{asset('css/app-light.css')}}" id="lightTheme">
+    <link rel="stylesheet" href="{{asset('css/app-dark.css')}}" id="darkTheme" disabled>
   </head>
   <body class="light ">
     <div class="wrapper vh-100">
@@ -77,16 +77,16 @@
         </form>
       </div>
     </div>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/moment.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/simplebar.min.js"></script>
-    <script src='js/daterangepicker.js'></script>
-    <script src='js/jquery.stickOnScroll.js'></script>
-    <script src="js/tinycolor-min.js"></script>
-    <script src="js/config.js"></script>
-    <script src="js/apps.js"></script>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/moment.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/simplebar.min.js')}}"></script>
+    <script src='{{asset('js/daterangepicker.js') }}'></script>
+    <script src='{{asset('js/jquery.stickOnScroll.js') }}'></script>
+    <script src="{{asset('js/tinycolor-min.js')}}"></script>
+    <script src="{{asset('js/config.js')}}"></script>
+    <script src="{{asset('js/apps.js')}}"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56159088-1"></script>
     <script>
