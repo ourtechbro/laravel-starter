1335b60890fcede078d335c0d5fcbcb68cf2bab4
Md. Emtiaz Zahid <emtiazzahid@gmail.com>
2024-02-29 21:59:48 +0600

    Sail (#21)
    integrate sail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,12 +17,12 @@
                 @csrf
                 <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="">
                     <svg version="1.1" id="logo" class="navbar-brand-img brand-md" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
-                  <g>
-                      <polygon class="st0" points="78,105 15,105 24,87 87,87" />
-                      <polygon class="st0" points="96,69 33,69 42,51 105,51" />
-                      <polygon class="st0" points="78,33 15,33 24,15 87,15" />
-                  </g>
-                </svg>
+                        <g>
+                            <polygon class="st0" points="78,105 15,105 24,87 87,87" />
+                            <polygon class="st0" points="96,69 33,69 42,51 105,51" />
+                            <polygon class="st0" points="78,33 15,33 24,15 87,15" />
+                        </g>
+                    </svg>
                 </a>
                 <h1 class="h6 mb-3">{{ __('Sign in') }}</h1>
                 <div class="form-group">
@@ -35,7 +35,8 @@
                 </div>
                 <div class="checkbox mb-3">
                     <label>
-                        <input type="checkbox" name="remember" value="remember-me"> {{ __('Remember me') }} </label>
+                        <input type="checkbox" name="remember" value="remember-me"> {{ __('Remember me') }}
+                    </label>
                 </div>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
